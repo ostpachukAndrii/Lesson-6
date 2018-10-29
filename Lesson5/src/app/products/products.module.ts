116103c6f8ff038dd7a77ee34c517a392c47f2bc
ostapchukandrii <ostapchukandrii@gmail.com>
2018-10-29 21:42:17 +0200

    fix

--- Lesson5/src/app/products/products.module.ts
+++ Lesson5/src/app/products/products.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BookComponent } from './components/book/book.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { AppHighlightDirective } from '../shared/directives/appHighLight/app-highlight.directive';
+
 
 @NgModule({
   imports: [
